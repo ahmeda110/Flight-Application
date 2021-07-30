bfd025def1bde14e89e4f86b499adc2b31eb716d
Ahmed-Abbas <65793945+ahmeda110@users.noreply.github.com>
2021-07-29 18:41:21 -0600

    Update Final_Project.h

--- Final_Project.h
+++ Final_Project.h
@@ -1,18 +1,13 @@
 #include <string>
 
-
 #ifndef Final_Project
 #define Final_Project
 
-
 void display_header();
 
-char *trimwhitespace(char *str);
-void cleanStandardInputStream (void);
-
-
+char * trimwhitespace(char * str);
+void cleanStandardInputStream(void);
 
 void pressEnter();
-
 
 #endif
