46c9609caf3e933de8288afa2801d6c80894b820
Ahmed-Abbas <65793945+ahmeda110@users.noreply.github.com>
2021-07-29 18:43:39 -0600

    Update Passenger.cpp

--- Passenger.cpp
+++ Passenger.cpp
@@ -1,70 +1,66 @@
 #include "Passenger.h"
-		
-Passenger::Passenger(){
-	first_name = (" ");
-	last_name = (" ");
-	phone_number = (" ");
-	seat_details = Seat();
-	id_number = 0;
+
+Passenger::Passenger() {
+  first_name = (" ");
+  last_name = (" ");
+  phone_number = (" ");
+  seat_details = Seat();
+  id_number = 0;
 }
 
-Passenger::Passenger(const string firstN, const string lastN, const string phoneN, const int seatN, const char seatL, const int id){
-	first_name = firstN;
-	last_name = lastN;
-	phone_number = phoneN;
-	seat_details = Seat(seatN, seatL);
-	id_number = id;}
+Passenger::Passenger(const string firstN,
+  const string lastN,
+    const string phoneN,
+      const int seatN,
+        const char seatL,
+          const int id) {
+  first_name = firstN;
+  last_name = lastN;
+  phone_number = phoneN;
+  seat_details = Seat(seatN, seatL);
+  id_number = id;
+}
 
 //setters
-void Passenger::setFirstName(const string firstN)
-{
-	first_name = firstN;
+void Passenger::setFirstName(const string firstN) {
+  first_name = firstN;
 }
 
-void Passenger::setLastName(const string lastN)
-{
-	last_name = lastN;
+void Passenger::setLastName(const string lastN) {
+  last_name = lastN;
 }
 
-void Passenger::setPhoneNumber(const string phoneN)
-{
-	phone_number = phoneN;
+void Passenger::setPhoneNumber(const string phoneN) {
+  phone_number = phoneN;
 }
 
-void Passenger::setSeatDetails(const int seatN, const char seatL)
-{
-	seat_details.setSeatLetter(seatL);
-	seat_details.setSeatNumber(seatN);
+void Passenger::setSeatDetails(const int seatN,
+  const char seatL) {
+  seat_details.setSeatLetter(seatL);
+  seat_details.setSeatNumber(seatN);
 }
 
-void Passenger::setIdNumber(const int idN)
-{
-	id_number = idN;
+void Passenger::setIdNumber(const int idN) {
+  id_number = idN;
 }
-
 
 //getters
-string Passenger::getFirstName() const
-{
-	return first_name;
+string Passenger::getFirstName() const {
+  return first_name;
 }
 
-string Passenger::getLastName() const
-{
-	return last_name;
+string Passenger::getLastName() const {
+  return last_name;
 }
 
-string Passenger::getPhoneNumber() const
-{
-	return phone_number;
+string Passenger::getPhoneNumber() const {
+  return phone_number;
 }
 
-Seat Passenger::getSeatDetails() const
-{
-	return seat_details;
+Seat Passenger::getSeatDetails() const {
+  return seat_details;
 }
 
-int Passenger::getIdNumber() const
-{
-	return id_number;
+int Passenger::getIdNumber() const {
+  return id_number;
 }
