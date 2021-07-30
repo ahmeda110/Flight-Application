0afb4d94d63db522a8c85fc00d19f658462bbd59
Ahmed-Abbas <65793945+ahmeda110@users.noreply.github.com>
2021-07-29 18:45:15 -0600

    Update Seat.cpp

--- Seat.cpp
+++ Seat.cpp
@@ -1,28 +1,27 @@
 #include "Seat.h"
 
-Seat::Seat()
-{
-	seatLetter = ' ';
-	seatNumber = 0;}
+Seat::Seat() {
+  seatLetter = ' ';
+  seatNumber = 0;
+}
 
-Seat::Seat(int seatN,char seatL)
-{
-	seatLetter = seatL;
-	seatNumber = seatN;}
+Seat::Seat(int seatN, char seatL) {
+  seatLetter = seatL;
+  seatNumber = seatN;
+}
 
-void Seat::setSeatLetter(char seatL)
-{
-	seatLetter=seatL;}
-	
-void Seat::setSeatNumber(int seatN)
-{
-	seatNumber=seatN;}
-	
-	
-char Seat::getSeatLetter() const
-{
-	return seatLetter;}
-	
-int Seat::getSeatNumber() const
-{
-	return seatNumber;}
+void Seat::setSeatLetter(char seatL) {
+  seatLetter = seatL;
+}
+
+void Seat::setSeatNumber(int seatN) {
+  seatNumber = seatN;
+}
+
+char Seat::getSeatLetter() const {
+  return seatLetter;
+}
+
+int Seat::getSeatNumber() const {
+  return seatNumber;
+}
