2cf1a95154e5b08c60ef02095dc81471b83d66f6
Ahmed-Abbas <65793945+ahmeda110@users.noreply.github.com>
2021-07-29 18:44:13 -0600

    Update Passenger.h

--- Passenger.h
+++ Passenger.h
@@ -1,36 +1,44 @@
 #include <iostream>
 #include <string>
+
 #include "Seat.h"
+
 using namespace std;
 
 #ifndef Passenger_Included
 #define Passenger_Included
 
-class Passenger{
+class Passenger {
 
-		public:
-		
-		void setFirstName(const string firstN);	
-		void setLastName(const string lastN);
-		void setPhoneNumber(const string phoneN);
-		void setSeatDetails(const int setN, const char seatL);
-		void setIdNumber(const int idN);
+  public:
 
-		Passenger(); 
-		Passenger(const string firstN, const string lastN, const string phoneN, const int seatN, const char seatL, const int id); 
-		string getFirstName() const;
-		string getLastName() const;
-		string getPhoneNumber() const; 
-		Seat getSeatDetails() const;
-		int getIdNumber() const; 
+    void setFirstName(const string firstN);
+  void setLastName(const string lastN);
+  void setPhoneNumber(const string phoneN);
+  void setSeatDetails(const int setN,
+    const char seatL);
+  void setIdNumber(const int idN);
 
-		
-		private:
+  Passenger();
+  Passenger(const string firstN,
+    const string lastN,
+      const string phoneN,
+        const int seatN,
+          const char seatL,
+            const int id);
+  string getFirstName() const;
+  string getLastName() const;
+  string getPhoneNumber() const;
+  Seat getSeatDetails() const;
+  int getIdNumber() const;
 
-		string first_name;
-		string last_name;
-		string phone_number;
-		Seat seat_details;
-		int id_number;
-	};
+  private:
+
+    string first_name;
+  string last_name;
+  string phone_number;
+  Seat seat_details;
+  int id_number;
+};
+
 #endif
